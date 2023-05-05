a2aecb45665183ea5edc8da438fcecad227c5a9d
Александр Ильин <ilin.aleksandr@vniizht.ru>
2023-05-05 20:53:59 +0300

    Working on

--- src/main/kotlin/mojo/DependenciesInstaller.kt
+++ src/main/kotlin/mojo/DependenciesInstaller.kt
@@ -8,13 +8,12 @@ import org.apache.maven.plugins.annotations.LifecyclePhase
 import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.Parameter
 import org.apache.maven.project.DefaultProjectBuildingRequest
-import org.apache.maven.project.ProjectBuildingRequest
 import org.apache.maven.repository.RepositorySystem
 import org.apache.maven.shared.dependency.graph.DependencyGraphBuilder
 import org.apache.maven.shared.dependency.graph.DependencyNode
 import org.intellij.lang.annotations.Language
-
 import java.io.File
+import java.util.jar.JarFile
 
 
 @Mojo(name = "install-to-wildfly", defaultPhase = LifecyclePhase.INSTALL)
@@ -32,13 +31,16 @@ class DependenciesInstaller : AbstractMojo() {
     @Parameter(required = true)
     lateinit var wildflyHome: String
 
+    @Parameter(defaultValue = "global")
+    lateinit var group: String
+
     @Parameter(defaultValue = "main")
     lateinit var slot: String
 
     override fun execute() {
         try {
             checkIfWildFlyExists()
-            installModules(buildDependencyGraph())
+            installModules()
         }
         catch (err: Error){
             log.error(err.message)
@@ -50,68 +52,60 @@ class DependenciesInstaller : AbstractMojo() {
         }
     }
 
-    private fun buildDependencyGraph(): DependencyNode {
-        val buildingRequest: ProjectBuildingRequest = DefaultProjectBuildingRequest(session.projectBuildingRequest)
-        buildingRequest.project = session.currentProject
-        return dependencyGraphBuilder.buildDependencyGraph(buildingRequest, null)
-    }
-
     private fun checkIfWildFlyExists() = with(File(wildflyHome)) {
         if(!exists())    throw Error("WildFly doesn't exist: $wildflyHome")
         if(!isDirectory) throw Error("WildFly location is not directory: $wildflyHome")
     }
 
-    private fun installModules(node: DependencyNode, depth: Int = 0) {
-        if (depth == 0)
-            log.info(node.toNodeString() + " modules installation:")
-        else {
-            resolveModule(node)
-            log.info("${"|   ".repeat(depth)}${node.artifact.file.name}")
-
-//            installModule(node)
-        }
-        for (child in node.children)
-            installModules(child, depth + 1)
-    }
-
-    private fun resolveModule(node: DependencyNode) {
-        val request = ArtifactResolutionRequest()
-        request.artifact = node.artifact
-        repositorySystem.resolve(request)
-    }
-
-    private fun installModule(node: DependencyNode) {
-        fun createModuleName(node: DependencyNode) = with(node.artifact){"$groupId.$artifactId.${version.replace('.', '-')}"}
-        val name = createModuleName(node)
-        val resource = node.artifact.file?.absolutePath
-        val dependencies = node.children.map { child -> createModuleName(child) }
-
-    }
+    private fun installModules() = Module(
+        dependencyGraphBuilder
+            .buildDependencyGraph(
+                DefaultProjectBuildingRequest(session.projectBuildingRequest)
+                    .apply { project = session.currentProject }, null)
+    ).install()
 
     inner class Module(node: DependencyNode, depth: Int = 0){
         init {
             repositorySystem.resolve(ArtifactResolutionRequest().apply { artifact = node.artifact })
         }
 
-        val name = with(node.artifact){"$groupId.$artifactId.${version.replace('.', '-')}"}
-        val absolutePath = node.artifact.file?.absolutePath
-        val dependencies = node.children.map { child -> Module(child) }
+        val name = with(node.artifact){
+            JarFile(file).manifest?.mainAttributes?.getValue("Automatic-Module-Name")
+                ?: "$groupId.$artifactId"
+        }
+        val home = "$wildflyHome/modules/${name.replace('.', '/')}/$group/$slot/"
+        val dependencies = node.children.map { child -> Module(child, depth+1) }
 
         @Language("xml")
         private val xml = """
              <module xmlns="urn:jboss:module:1.1" name="$name">
                  <resources>
-                     <resource-root path="postgresql-42.6.0.jar"/>
+                     <resource-root path="${node.artifact.file.name}"/>
                  </resources>
-
                  <dependencies>
-                     <module name="global.org.checkerframework.qualddd"/>
+                     ${dependencies.joinToString("""
+                     """) { dependency -> "<module name=\"${dependency.name}\"/>" }}
                  </dependencies>
              </module>
         """.trimIndent()
 
-        fun install(){
+        fun install(logDepth: Int = 0){
+            log.info(
+                if(logDepth == 0) "${name} modules installation:"
+                else xml//"|   ".repeat(logDepth) + name
 
+            )
+            for (dependency in dependencies)
+                dependency.install(logDepth + 1)
         }
+
+        fun copyFileToPath(path: String) {
+            val newFile = File(path)
+            if (!newFile.exists()) {
+                newFile.createNewFile()
+            }
+//            Files.copy(no.toPath(), newFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+        }
+
     }
 }
